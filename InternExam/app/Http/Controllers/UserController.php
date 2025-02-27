4f8e23be4fb7572031248807de1da3c494e36ecb
karldenniss <134616669+karldenniss@users.noreply.github.com>
2025-02-27 18:05:14 +0800

    UserController not yet done

---
+++ InternExam/app/Http/Controllers/UserController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //Display a list of all users.
+    public function index(){
+        
+    }
+    //Display a form to create a new user.
+    public function create(){
+        
+    }
+    //Store a new user in the database.
+    public function store(){
+        
+    }
+    //Display a form to edit an existing user.
+    public function edit(){
+        
+    }
+
+    //Update an existing user in the database.
+    public function update(){
+        
+    }
+    // Update an existing user in the database.
+    public function destroy(){
+        
+    }
+    //Display a list of all active users.
+    public function activeUsers(){
+        
+    }
+}
